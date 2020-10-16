1b3a7045169ba4dc020dadd3bfeb35efbc76e965
Hussein Taher <hussein_taher@outlook.com>
2020-10-16 20:17:22 +0200

    use vulkan github repos as dependencies

---
+++ thirdparty/vulkan.cmake
@@ -0,0 +1,31 @@
+# Vulkan-Headers
+message("-- External Project: Vulkan-Headers")
+include(FetchContent)
+
+FetchContent_Declare(
+    vulkan_headers
+    GIT_REPOSITORY  https://github.com/KhronosGroup/Vulkan-Headers.git
+    GIT_TAG         v1.2.157
+)
+
+FetchContent_MakeAvailable(vulkan_headers)
+
+# Vulkan-Loader
+message("-- External Project: Vulkan-Loader")
+include(FetchContent)
+
+FetchContent_Declare(
+    vulkan_loader
+    GIT_REPOSITORY  https://github.com/KhronosGroup/Vulkan-Loader.git
+    GIT_TAG         v1.2.157
+)
+
+FetchContent_MakeAvailable(vulkan_loader)
+
+set_target_properties("asm_offset" PROPERTIES FOLDER "thirdparty/vulkan")
+set_target_properties("loader-norm" PROPERTIES FOLDER "thirdparty/vulkan")
+set_target_properties("loader-opt" PROPERTIES FOLDER "thirdparty/vulkan")
+set_target_properties("loader-unknown-chain" PROPERTIES FOLDER "thirdparty/vulkan")
+set_target_properties("vulkan" PROPERTIES FOLDER "thirdparty/vulkan")
+set_target_properties("VulkanLoader_generated_source" PROPERTIES FOLDER "thirdparty/vulkan")
+set_target_properties("loader_asm_gen_files" PROPERTIES FOLDER "thirdparty/vulkan")
